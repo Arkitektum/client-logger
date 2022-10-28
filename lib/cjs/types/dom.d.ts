c1567a187a2b10b646f9eca95bbe264d456ac741
Benjamin Dehli <superelg@gmail.com>
2022-10-28 18:35:27 +0200

    Remove build files

--- lib/cjs/types/dom.d.ts
+++
@@ -1,6 +0,0 @@
-declare global {
-    interface Window {
-        sourceMap: any;
-    }
-}
-export declare function addScript(): Promise<unknown>;
--- lib/cjs/types/functions.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function getErrorDataFromError(error: Error, consumer: any): {
-    message: string;
-    originalPosition: any;
-};
-export declare function getErrorDataFromSourceMap(event: Event | string, source?: string, line?: number, column?: number, consumer?: any): {
-    message: string;
-    originalPosition: any;
-};
